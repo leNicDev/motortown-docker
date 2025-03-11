3ee7a0b4c838feed57980e17668a11595fcda092
leNicDev <germanelectronix@gmail.com>
2025-03-11 20:02:59 +0000

    force platform type

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 mkdir -p "${STEAM_COMPAT_DATA_PATH}"
 
 # install or update server and verify files
-/opt/steamcmd/steamcmd.sh +force_install_dir /app +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +app_update 1007 validate +app_update ${STEAM_APP_ID} -beta test -betapassword motortowndedi validate +quit
+/opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /app +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +app_update 1007 validate +app_update ${STEAM_APP_ID} -beta test -betapassword motortowndedi validate +quit
 
 # start server
 proton run /app/MotorTown/Binaries/Win64/MotorTownServer-Win64-Shipping.exe Jeju_World?listen? -server -log -useperfthreads
