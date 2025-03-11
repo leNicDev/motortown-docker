3c9cb2ec28d2edd49114d963a460a0ab4d1ae0b6
leNicDev <germanelectronix@gmail.com>
2025-03-11 20:26:46 +0000

    copy libs

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,8 +9,7 @@ mkdir -p "${STEAM_COMPAT_DATA_PATH}"
 /opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /app +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +app_update 1007 validate +app_update ${STEAM_APP_ID} -beta test -betapassword motortowndedi validate +quit
 
 # copy steamclient.dll
-cp /app/steamclient.dll /app/MotorTown/Binaries/Win64/
-cp /app/steamclient64.dll /app/MotorTown/Binaries/Win64/
+cp /app/*.dll /app/MotorTown/Binaries/Win64/
 
 # start server
 proton run /app/MotorTown/Binaries/Win64/MotorTownServer-Win64-Shipping.exe Jeju_World?listen? -server -log -useperfthreads
